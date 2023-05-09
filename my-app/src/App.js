456ec5a99412ed9b01ce84df324346550d158ea4
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-05-09 17:21:15 +0200

    State - Create a `Counter` class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Welcome from './components/Welcome';
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
     
       <div>
       <h1>Welcome to my super app!</h1>
-      <Welcome name={"John"} age={17}/>
+      <Counter/>
     </div>
   
     </div>
---
+++ my-app/src/components/Counter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0
+        };
+      }
+      
+      componentDidMount() {
+        this.interval = setInterval(() => {
+          this.setState(prevState => ({
+            count: prevState.count + 1
+          }));
+        }, 1000);
+      }
+    
+      componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+      
+  render() {
+    return (
+      <div>
+        <h1>This is the count: {this.state.count}</h1>
+      </div>
+    );
+  }
+}
+
+
+  
+export default Counter;
