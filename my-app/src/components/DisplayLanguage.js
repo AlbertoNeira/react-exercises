122d697105969537f2f1963799641bfd51c63f76
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-05-14 19:13:32 +0200

    useContext

--- my-app/src/components/DisplayLanguage.js
+++ my-app/src/components/DisplayLanguage.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import LanguageContext from './LanguageContext';
 
 const DisplayLanguage = () => {
+  const selectedLanguage = useContext(LanguageContext);
+
   return (
-    <LanguageContext.Consumer>
-      {(selectedLanguage) => (
-        <div>
-          <h2>Display Language:</h2>
-          <p>{selectedLanguage}</p>
-        </div>
-      )}
-    </LanguageContext.Consumer>
+    <div>
+      <h1>Selected Language: {selectedLanguage}</h1>
+    </div>
   );
 };
 
