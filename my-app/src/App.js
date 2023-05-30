de6c859cd57d2e0b6b3065d223384a8f5f440e3f
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-05-30 17:15:10 +0200

    React Router - Add three Links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,28 +9,33 @@ import ShowGithubUser from './components/ShowGithubUser';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/counter">Counter</Link>
-              </li>
-              <li>
-                <Link to="/users/AlbertoNeira">Show Alberto Neira</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/users/:username" element={<ShowGithubUser />} />
-            <Route path="/counter" element={<Counter />} />
-          </Routes>
-        </div>
+    <div className="App">
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+            <li>
+              <Link to="/users/AlbertoNeira">Show Alberto Neira</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+            <Route path="/login" element={<Login />} /> 
+          <Route path="/users/:username" element={<ShowGithubUser />} />
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
       </div>
-    </Router>
+  </div>
+  
+  </Router>
   );
 }
 
